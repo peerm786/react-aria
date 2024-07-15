a5e2a725cfd8d65e08d7e788f04f8e3d0fae110b
Selva-Torus <157101986+Selva-Torus@users.noreply.github.com>
2024-07-15 10:29:58 +0530

    dropdown changes

---
+++ app/components/multiDropdownnew.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+import {
+  Button,
+  Dialog,
+  DialogTrigger,
+  Popover,
+  ListBox,
+  ListBoxItem,
+} from "react-aria-components";
+import { IoIosCheckmark } from "react-icons/io";
+import { twMerge } from "tailwind-merge";
+import { DownArrow } from "../constants/svgApplications";
+
+type classNames = {
+  triggerButton?: string;
+  popover?: string;
+  listbox?: string;
+  listboxItem?: string;
+};
+
+interface CustomDropDpwnProps {
+  triggerButton: React.ReactNode;
+  multiple?: boolean;
+  selectedKeys: any[];
+  setSelectedKeys: (keys: any) => void;
+  items: any[];
+  displayParam?: string; 
+  classNames?: classNames;
+  renderOption?: (
+    item: any,
+    close: () => void,
+    handleSelectionChange: (selectedItems: any, close: () => void) => void,
+    setOpen : (open: boolean) => void
+  ) => React.ReactNode;
+}
+
+const DropDown = ({
+  triggerButton,
+  multiple = false,
+  selectedKeys,
+  setSelectedKeys,
+  items,
+  displayParam,
+  classNames,
+  renderOption,
+}: CustomDropDpwnProps) => {
+  
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleSelectionChange = (selectedItem: any, close: () => void) => {
+    if (multiple) {
+      setSelectedKeys((prevKeys: any[]) =>
+        prevKeys.some((k: any) =>
+          displayParam
+            ? k[displayParam] === selectedItem[displayParam]
+            : k === selectedItem
+        )
+          ? prevKeys.filter((k: any) =>
+              displayParam
+                ? k[displayParam] !== selectedItem[displayParam]
+                : k !== selectedItem
+            )
+          : [...prevKeys, selectedItem]
+      );
+    } else {
+      setSelectedKeys([selectedItem]);
+      close();
+      setOpen(false);
+    }
+  };
+
+  const getItemDisplayValue = (item: any) => {
+    return displayParam ? item[displayParam] : item;
+  };
+
+  return (
+    <DialogTrigger >
+      <Button
+        className={twMerge(
+          `p-2 items-center flex justify-between rounded focus:outline-none w-full ${open ? "border-[#0736C4]": ""}`,
+          classNames?.triggerButton
+        )}
+        onPress={() => setOpen(!open)}
+      >
+        {selectedKeys.length
+          ? selectedKeys
+              .map((item: any) => getItemDisplayValue(item))
+              .join(", ")
+          : triggerButton}
+          {<DownArrow />}
+      </Button>
+      <Popover
+        placement="bottom"
+        className={twMerge("w-full", classNames?.popover)}
+      >
+        <Dialog className="border bg-white focus:outline-none rounded-lg">
+          {({ close }) => (
+            <ListBox
+              aria-label="Custom dropdown"
+              selectionMode={multiple ? "multiple" : "single"}
+              className={twMerge("", classNames?.listbox)}
+            >
+              {items.map((item: any) => {
+                const isSelected = selectedKeys.some((k: any) =>
+                  displayParam
+                    ? k[displayParam] === item[displayParam]
+                    : k === item
+                );
+                if (renderOption) {
+                  return renderOption(item, close, handleSelectionChange , setOpen);
+                } else {
+                  return (
+                    <ListBoxItem
+                      key={displayParam ? item[displayParam] : item}
+                      textValue={getItemDisplayValue(item)}
+                      onAction={() => handleSelectionChange(item, close)}
+                      className={twMerge(
+                        `focus:outline-none p-2 ${
+                          isSelected ? "bg-[#F9FAFB]" : ""
+                        }`,
+                        classNames?.listboxItem
+                      )}
+                    >
+                      {getItemDisplayValue(item)}
+                      {isSelected && <IoIosCheckmark fill="blue" />}
+                    </ListBoxItem>
+                  );
+                }
+              })}
+            </ListBox>
+          )}
+        </Dialog>
+      </Popover>
+    </DialogTrigger>
+  );
+};
+
+export default DropDown;
