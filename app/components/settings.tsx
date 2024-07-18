cdd0f3b0de472e380436fc98c6ad5e33a9af2334
Selva-Torus <157101986+Selva-Torus@users.noreply.github.com>
2024-07-18 10:59:14 +0530

    new changes

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -1,18 +1,36 @@
 import React from "react";
-import { AccountSettingsIcon, AppearanceIcon, AppSetupIcon, LinkedAccountsIcon, TenantSetupIcon, UserSettingsIcon } from "../constants/svgApplications";
+import {
+  AccountSettingsIcon,
+  AppearanceIcon,
+  AppSetupIcon,
+  LinkedAccountsIcon,
+  TenantSetupIcon,
+  UserSettingsIcon,
+} from "../constants/svgApplications";
 
 const Settings = () => {
-  const MenuItems = [
-    { icon: TenantSetupIcon, name: "Tenant Setup" },
-    { icon: AppSetupIcon, name: "App Setup" },
-    {icon : UserSettingsIcon , name : "User Settings"},
-    {icon : AppearanceIcon , name:"Appearance"},
-    {icon : AccountSettingsIcon , name : "Account Settings"},
-    {icon : LinkedAccountsIcon , name : "Linked Accounts"},
+  const SettingsMenu = [
+    { Icon: TenantSetupIcon, name: "Tenant Setup" },
+    { Icon: AppSetupIcon, name: "App Setup" },
+    { Icon: UserSettingsIcon, name: "User Settings" },
+    { Icon: AppearanceIcon, name: "Appearance" },
+    { Icon: AccountSettingsIcon, name: "Account Settings" },
+    { Icon: LinkedAccountsIcon, name: "Linked Accounts" },
   ];
   return (
     <div className="p-2 w-[45vw]">
       <h1 className="w-full border-b">Settings</h1>
+      <div className="flex">
+        <section aria-label="SettingsMenu" className="flex flex-col gap-3 first:mt-2">
+          {SettingsMenu.map(({ Icon, name }, index) => (
+            <div className={`w-full flex gap-2 text-sm ${
+                index === 2 ? "border-b" : ""
+              }`}>
+                <Icon />{name}
+            </div>
+          ))}
+        </section>
+      </div>
     </div>
   );
 };
