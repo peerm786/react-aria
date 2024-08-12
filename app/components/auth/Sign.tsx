b84a4f09cd2146017138864c3f02f19c25ca2ae8
Selva-Torus <157101986+Selva-Torus@users.noreply.github.com>
2024-08-12 19:34:20 +0530

    UPDATED CHANGES

--- app/components/auth/Sign.tsx
+++ app/components/auth/Sign.tsx
@@ -3,55 +3,53 @@ import React from "react";
 import Image from "next/image";
 import DashBoard from "../../constants/image.png";
 import DarkModeDashboard from "../../constants/darkDashboard.png";
-import LoginForm from "../../components/auth/startpage"
+import LoginForm from "../../components/auth/startpage";
 import { ArrowForward } from "../../constants/svgApplications";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../lib/Store/store";
 
 const Login = () => {
-    const isDarkMode = useSelector((state: RootState) => state.main.useDarkMode);
+  const isDarkMode = useSelector((state: RootState) => state.main.useDarkMode);
 
-    return (
-        <div className="flex justify-between w-full h-screen bg-[#F4F4F6] overflow-hidden  dark:bg-[#171717] transition-colors duration-700 ease">
-            <div className="w-[30%] flex items-center justify-center  ">
-                <LoginForm
-                />
+  return (
+    <div className="flex justify-between w-full h-screen bg-[#F4F4F6] overflow-hidden  dark:bg-[#171717] transition-colors duration-700 ease">
+      <div className="w-[30%] flex items-center justify-center  ">
+        <LoginForm />
+      </div>
+      <div
+        className={`flex flex-col justify-between w-[66%] h-[100%] pt-[3vw] gap-[2vw]`}
+      >
+        <div className="flex flex-col">
+          <div className="flex items-center justify-between mr-8">
+            <div className="flex gap-1 items-center">
+              <span className="w-[.46vw] h-[.46vw] rounded-full bg-[#0736C4]"></span>
+              <p className="text-[0.93vw] leading-[2vh] font-bold text-[#0736C4] dark:text-white">
+                What's New
+              </p>
             </div>
-            <div
-                className={`flex flex-col justify-between w-[66%] h-[100%] `}>
-                <div className="flex flex-col mt-[5%]  ">
+            <p className="flex items-center text-[#0736C4] dark:text-white text-[0.72vw] font-medium leading-[1.5vh]">
+              View all changes <ArrowForward />
+            </p>
+          </div>
 
-                    <div className="flex items-center justify-between mr-8">
-                        <div className="flex gap-1 items-center">
-                            <span className="w-[.46vw] h-[.46vw] rounded-full bg-[#0736C4]">
-                            </span>
-                            <p className="text-[0.93vw] leading-[2vh] font-bold text-[#0736C4] dark:text-white">
-                                What's New
-                            </p>
-                        </div>
-                        <p className="flex text-[#0736C4] dark:text-white text-[0.72vw] font-medium leading-[1.5vh]">
-                            View all changes <ArrowForward />
-                        </p>
-                    </div>
-
-                    <p className="flex pt-3 dark:text-white text-[1.8vw] font-bold leading-[3.92vh]">
-                        Discover the New Torus 9
-                    </p>
-                    <p className="text-[0.93vw] leading-[2.31vh] w-[75%] mt-4 text-black/50 dark:text-[#FFFFFF]/50">
-                        Experience the all new Torus9 with an enhanced UI Intuitive appflow,newscreens
-                        a refreshed home and our newbrand new marketplace and templates Log in into
-                        Explore the nextlevel of productivity and innovations
-                    </p>
-
-                </div>
-                <Image
-                    className="w-[100%] h-[65vh] mr-auto rounded-tl-[3.5%]  "
-                    src={isDarkMode ? DarkModeDashboard : DashBoard}
-                    alt="bankmaster"
-                />
-            </div>
+          <p className="flex pt-3 dark:text-white text-[1.8vw] font-bold leading-[3.92vh]">
+            Discover the New Torus 9
+          </p>
+          <p className="text-[0.93vw] leading-[2.31vh] w-[75%] mt-4 text-black/50 dark:text-[#FFFFFF]/50">
+            Experience the all new Torus9 with an enhanced UI Intuitive
+            appflow,newscreens a refreshed home and our newbrand new marketplace
+            and templates Log in into Explore the nextlevel of productivity and
+            innovations
+          </p>
         </div>
-    );
+        <Image
+          className="w-[100%] mr-auto rounded-tl-[3.5%]  "
+          src={isDarkMode ? DarkModeDashboard : DashBoard}
+          alt="bankmaster"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- app/components/auth/startpage.tsx
+++ app/components/auth/startpage.tsx
@@ -76,7 +76,10 @@ function LoginForm({ variant = "TP" }: LoginFormProps) {
         if (res?.error) {
           setLoading(false);
           toast(
-            <TorusToast setWordLength={setWordLength} wordLength={wordLength} />,
+            <TorusToast
+              setWordLength={setWordLength}
+              wordLength={wordLength}
+            />,
             {
               type: "error",
               position: "bottom-right",
@@ -86,10 +89,13 @@ function LoginForm({ variant = "TP" }: LoginFormProps) {
               text: `Failed to login, check credentials`,
               closeButton: false,
             } as any
-          )
+          );
         } else {
           toast(
-            <TorusToast setWordLength={setWordLength} wordLength={wordLength} />,
+            <TorusToast
+              setWordLength={setWordLength}
+              wordLength={wordLength}
+            />,
             {
               type: "success",
               position: "bottom-right",
@@ -114,7 +120,7 @@ function LoginForm({ variant = "TP" }: LoginFormProps) {
             text: `Please fill all the fields`,
             closeButton: false,
           } as any
-        )
+        );
         setLoading(false);
       }
     } catch (error) {
@@ -138,44 +144,45 @@ function LoginForm({ variant = "TP" }: LoginFormProps) {
   };
 
   return (
-    <div className="min-h-screen w-full bg-[#FFFFFF] border-none dark:bg-[#000000]">
-      <h2 className="flex leading-[2.6vh] text-[1.25vw] p-3 items-center font-medium text-black dark:text-white">
+    <div className="min-h-screen w-full flex justify-between py-[2vw] flex-col bg-[#FFFFFF] border-none dark:bg-[#000000]">
+      <h2 className="flex leading-[2.6vh] text-[1.25vw] p-3 items-center font-medium text-black dark:text-white transition-transform duration-700 ease-in-out">
         <TorusLogo /> TORUS
       </h2>
-
-      <Form className="flex flex-col h-full space-y-4 justify-center px-12 transition-all duration-700 ease-in-out">
-        <div className="flex flex-col pt-10">
+      <div className="flex flex-col gap-[2vw] px-[16.5%]">
+        <div className="flex flex-col">
           <h1 className="text-[2vw] leading-[4.4vh] font-semibold text-black dark:text-white">
             Log in
           </h1>
-          <p className="text-[#000000]/35 dark:text-white/35 pt-2 text-[0.8vw] leading-[1.7vh]">
+          <p className="text-[#000000]/35 dark:text-white/35 pt-[0.6vw] text-[0.8vw] leading-[1.7vh]">
             Enter your details to get started
           </p>
         </div>
-        <div className="flex w-full bg-[#F4F5FA] dark:bg-[#161616] rounded-lg p-1 mt-6">
+
+        <div className="flex w-full bg-[#F4F5FA] dark:bg-[#161616] rounded-lg p-1 ">
           <Button
             onPress={() => setSelectedOption("Individual")}
-            className={`py-2 w-1/2 transition-colors duration-700 ease-in-out text-[0.8vw] font-medium leading-[1.8vh]
-                             ${selectedOption === "Individual"
-                ? "bg-[#FFFFFF] dark:bg-[#000] outline-none"
-                : " bg-inherit outline-none"
-              } dark:text-white text-black`}
+            className={`py-2 w-1/2 transition-colors duration-700 ease-in-out text-[0.8vw]  leading-[1.8vh] rounded-md
+                             ${
+                               selectedOption === "Individual"
+                                 ? "bg-[#FFFFFF] dark:bg-[#000] outline-none font-medium"
+                                 : " bg-inherit font-semibold"
+                             } dark:text-white text-black outline-none`}
           >
             Individual
           </Button>
           <Button
             onPress={() => setSelectedOption("Teams")}
-            className={`py-2 w-1/2 transition-colors duration-700 ease-in-out text-[0.8vw] font-medium leading-[1.8vh]
-                            ${selectedOption === "Teams"
-                ? "bg-[#FFFFFF] dark:bg-[#000] outline-none"
-                : " bg-inherit outline-none"
-              } dark:text-white text-black`}
+            className={`py-2 w-1/2 transition-colors duration-700 ease-in-out text-[0.8vw]  leading-[1.8vh] rounded-md
+                            ${
+                              selectedOption === "Teams"
+                                ? "bg-[#FFFFFF] dark:bg-[#000]  font-medium"
+                                : " bg-inherit outline-none font-semibold"
+                            } dark:text-white text-black outline-none`}
           >
             Team
           </Button>
         </div>
-
-        <div className="space-y-4 ">
+        <Form className="flex flex-col h-full justify-evenly transition-all duration-700 ease-in-out gap-[1vw]">
           {selectedOption === "Teams" && (
             <div className="flex flex-col">
               <Label
@@ -191,7 +198,7 @@ function LoginForm({ variant = "TP" }: LoginFormProps) {
                 items={clientList}
                 classNames={{
                   triggerButton:
-                    "w-full bg-[#F4F5FA] text-black rounded-lg text-[0.8vw] font-medium leading-[1.7vh] py-3 mt-2 dark:bg-[#171717] dark:text-[#FFFFFF] ",
+                    "w-full bg-[#F4F5FA] text-black rounded-lg text-[0.8vw] font-medium leading-[1.7vh] py-[1vw] mt-2 dark:bg-[#171717] dark:text-[#FFFFFF] ",
                   popover: "w-[20%]",
                 }}
               />
@@ -211,7 +218,7 @@ function LoginForm({ variant = "TP" }: LoginFormProps) {
               type="text"
               onChange={handleFormDataChange}
               placeholder="eg:support@torus.com"
-              className="bg-[#F4F5FA] text-[0.8vw] font-medium leading-[1.7vh] outline-none pl-3 dark:bg-[#171717] dark:text-[#FFFFFF] text-[#000000] py-3 rounded-md w-full "
+              className="bg-[#F4F5FA] text-[0.8vw] font-medium leading-[1.7vh] outline-none pl-3 dark:bg-[#171717] dark:text-[#FFFFFF] text-[#000000] py-[1vw] rounded-md w-full "
             />
           </div>
           <div className="flex flex-col relative">
@@ -228,10 +235,10 @@ function LoginForm({ variant = "TP" }: LoginFormProps) {
               type={showPassword ? "text" : "password"}
               onKeyUp={(e) => e.key === "Enter" && handleFormSubmit()}
               placeholder="Enter Password"
-              className="bg-[#F4F5FA] pl-3 outline-none text-[0.8vw] font-medium leading-[1.7vh] dark:text-[#FFFFFF] text-[#000000] dark:bg-[#171717] py-3 rounded-md w-full"
+              className="bg-[#F4F5FA] pl-3 outline-none text-[0.8vw] font-medium leading-[1.7vh] dark:text-[#FFFFFF] text-[#000000] dark:bg-[#171717] py-[1vw] rounded-md w-full"
             />
             <span
-              className="absolute bottom-8 right-4 "
+              className="absolute bottom-[2.5vw] right-[1.25vw] cursor-pointer"
               onClick={passwordvisible}
             >
               {showPassword ? (
@@ -244,52 +251,52 @@ function LoginForm({ variant = "TP" }: LoginFormProps) {
               Forgot Password?
             </p>
           </div>
-        </div>
-        <div className="flex justify-center mt-5 ">
-          <Button
-            onPress={handleFormSubmit}
-            isDisabled={loading}
-            className="bg-[#0736C4] w-full flex justify-center text-white px-4 py-4 text-[0.8vw] font-semibold leading-[1.7vh] disabled:bg-[#8c9ac4] disabled:cursor-not-allowed focus:outline-none rounded-lg"
-          >
-            {loading ? <ProgressButton isIndeterminate /> : "Sign In"}
-          </Button>
-        </div>
+          <div className="flex justify-center">
+            <Button
+              onPress={handleFormSubmit}
+              isDisabled={loading}
+              className="bg-[#0736C4] w-full flex justify-center text-white py-[1.3vw] text-[0.8vw] font-semibold leading-[1.7vh] disabled:bg-[#8c9ac4] disabled:cursor-not-allowed focus:outline-none rounded-lg"
+            >
+              {loading ? <ProgressButton isIndeterminate /> : "Sign In"}
+            </Button>
+          </div>
+        </Form>
         {selectedOption == "Individual" && (
-          <div className="flex items-center justify-center dark:text-white mt-5 w-full">
+          <div className="flex items-center justify-center dark:text-white w-full">
             <span className="h-px bg-black/15 w-[50%] "></span>
-            <span className="dark:text-white px-2 text-[0.8vw] leading-[1.7vh]">Or</span>
+            <span className="dark:text-white px-2 text-[0.8vw] leading-[1.7vh]">
+              Or
+            </span>
             <span className="h-px bg-black/15 w-[50%]"></span>
           </div>
         )}
         {selectedOption === "Individual" && (
-          <div className="flex w-full justify-between ">
+          <div className="flex w-full justify-between gap-[0.5vw]">
             <Button
-              onPress=
-              {handlesociallogin}
-
-              className="bg-[#F4F5FA] py-3 dark:bg-[#171717] dark:text-white text-[0.8vw] font-medium leading-[1.7vh] px-6 flex items-center justify-center focus:outline-none rounded-lg"
+              onPress={handlesociallogin}
+              className="bg-[#F4F5FA] w-full py-[1vw] dark:bg-[#171717] dark:text-white text-[0.8vw] font-medium leading-[1.7vh] px-6 flex items-center justify-center focus:outline-none rounded-lg"
             >
               <Gitbutton fill={isDarkMode ? "white" : "black"} />
               GitHub
             </Button>
             <Button
               onPress={handlesociallogin}
-              className="bg-[#F4F5FA] py-3 text-[0.8vw] font-medium leading-[1.7vh] dark:bg-[#171717] dark:text-white px-6 flex items-center justify-center focus:outline-none rounded-lg"
+              className="bg-[#F4F5FA] w-full py-[1vw] text-[0.8vw] font-medium leading-[1.7vh] dark:bg-[#171717] dark:text-white px-6 flex items-center justify-center focus:outline-none rounded-lg"
             >
               <Googlebutton />
               Google
             </Button>
           </div>
         )}
-        <div className="text-center text-[0.8vw] leading-[1.7vh]">
-          <p className="text-black/35 dark:text-white">
-            Don't have an account?{" "}
-            <a href="#" className="text-black dark:text-white font-bold">
-              Sign Up
-            </a>
-          </p>
-        </div>
-      </Form>
+      </div>
+      <div className="text-center text-[0.8vw] leading-[1.7vh]">
+        <p className="text-black/35 dark:text-white">
+          Don't have an account?{" "}
+          <a href="#" className="text-black dark:text-white font-bold">
+            Sign Up
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
